ec8e9394bb0cb725ee88a54bd57cf5f033c7ee70
aleviannaf <alexandrevianna511@gmail.com>
2023-03-16 13:38:59 -0400

    feature(post)

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,48 @@
-import express, {
-  Application,
-  json,
-  request,
-  Request,
-  Response,
-} from "express";
+import express, { Application, json, Request, Response } from "express";
+import { v4 as uuidv4} from 'uuid';
 
 const app: Application = express();
 app.use(json());
 
-app.post("/", (resquest: Request, response: Response): Response => {
-  return response.status(201).json(resquest.body);
+interface IWorkOrderRequest {
+  description: string
+  mechanical: string
+  client: string
+  price: number
+}
+
+interface IWorkOrder extends IWorkOrderRequest {
+  start_date: Date
+  end_date: Date
+  id: string
+}
+
+const orders: Array<IWorkOrder> = [];
+ 
+app.post("/work-order", (request: Request, response: Response): Response => {
+  
+  const orderDate: IWorkOrderRequest = request.body
+  
+
+  const newOrderData: IWorkOrder ={
+    id: uuidv4(),
+    start_date: new Date(),
+    end_date: new Date(Date.now() + 86400 * 1000),
+    ...orderDate
+  }
+
+  orders.push(newOrderData)
+
+  console.log(newOrderData)
+
+  return response.status(201).json(newOrderData);
 });
 
-app.get("/", (request: Request, response: Response): Response => {
-  return response.status(200).json({ status: "0k" });
+app.get("/work-order", (request: Request, response: Response): Response => {
+  return response.status(200).json(orders);
 });
-
 
 const PORT: number = 3000;
 
 const runnngiMsg: string = `Server running on http://localhost:${PORT}`;
-app.listen(PORT, ()=> console.log(runnngiMsg))
+app.listen(PORT, () => console.log(runnngiMsg));
